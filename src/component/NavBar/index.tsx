b0403053848e2670f92e1ab18c7ee08f4283c76a
yejiechen <1940879828@qq.com>
2025-01-27 13:41:27 +0800

    feat: 修改导航栏样式

--- src/component/NavBar/index.tsx
+++ src/component/NavBar/index.tsx
@@ -4,24 +4,26 @@ import Link from "next/link"
 const NavBar = () => {
   return (
     <div className="navbar bg-base-100">
-      <div className="flex-1">
-        <Link href="/" className="btn btn-ghost text-xl">
-          🌳TaroBlog
-        </Link>
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <Link href="/">路线图</Link>
-          </li>
-          <li>
-            <Link href="/notes">笔记</Link>
-          </li>
-          <li>
-            <Link href="/about">关于</Link>
-          </li>
-        </ul>
-      </div>
-      <div className="flex-none">
-        <ThemeChanger />
+      <div className="w-container flex items-center">
+        <div className="flex-1">
+          <Link href="/" className="btn btn-ghost text-xl">
+            🌳TaroBlog
+          </Link>
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <Link href="/">路线图</Link>
+            </li>
+            <li>
+              <Link href="/notes">笔记</Link>
+            </li>
+            <li>
+              <Link href="/about">关于</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="flex-none">
+          <ThemeChanger />
+        </div>
       </div>
     </div>
   )
