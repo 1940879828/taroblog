f14fa13d20ac24ff10a6155334ebeb43d8cbf982
yejiechen <1940879828@qq.com>
2025-01-30 22:35:46 +0800

    feat: 解决调整文章编码不正确找不到的问题

--- src/app/note/[name]/page.tsx
+++ src/app/note/[name]/page.tsx
@@ -9,15 +9,21 @@ import "md-editor-rt/lib/preview.css"
 export async function generateStaticParams() {
   const notesDir = path.join(process.cwd(), "public/notes")
   const fileNames = fs.readdirSync(notesDir)
-
-  return fileNames.map((fileName) => ({
-    name: fileName.replace(/\.md$/, "") // 去掉 .md 后缀
-  }))
+  return fileNames.map((fileName) => {
+    // 解码文件名
+    const decodedName = encodeURIComponent(fileName.replace(/\.md$/, ""))
+    console.log({decodedName})
+    return {
+      name: decodedName
+    }
+  })
 }
 
 // 获取 Markdown 文件内容
 async function getMarkdownContent(name: string) {
-  const filePath = path.join(process.cwd(), "public/notes", `${name}.md`)
+  const decodedName = decodeURIComponent(name) // 将路径参数编码回来
+  console.log({decodedName})
+  const filePath = path.join(process.cwd(), "public/notes", `${decodedName}.md`)
   const fileContent = fs.readFileSync(filePath, "utf8")
   const { content } = matter(fileContent) // 解析 Markdown 内容
   return marked(content)
