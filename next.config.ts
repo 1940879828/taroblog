e060b8b26570fb51316dad1f504ed6b17868663b
YeJieChen <1940879828@qq.com>
2025-02-06 14:18:19 +0800

    feat: 配置basePath

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
   output: "export", // 启用静态导出
   // 如果你的项目使用了动态路由，需要配置 basePath 和 assetPrefix
-  basePath: process.env.NODE_ENV === "production" ? "/your-repo-name" : "", // GitHub Pages 的仓库名称
-  assetPrefix: process.env.NODE_ENV === "production" ? "/your-repo-name/" : "" // GitHub Pages 的仓库名称
+  basePath: process.env.NODE_ENV === "production" ? "/taroblog-git-master-1940879828s-projects.vercel.app" : "", // GitHub Pages 的仓库名称
+  assetPrefix: process.env.NODE_ENV === "production" ? "/taroblog-git-master-1940879828s-projects.vercel.app/" : "" // GitHub Pages 的仓库名称
 }
 
 export default nextConfig
