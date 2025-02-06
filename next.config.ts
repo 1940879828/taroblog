1cfe02d81ce4adbc34eed01f26d59366669e9d81
YeJieChen <1940879828@qq.com>
2025-02-06 14:24:45 +0800

    feat: 还原nextConfig配置

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  output: "export", // 启用静态导出
-  // 如果你的项目使用了动态路由，需要配置 basePath 和 assetPrefix
-  basePath: process.env.NODE_ENV === "production" ? "/taroblog-git-master-1940879828s-projects.vercel.app" : "", // GitHub Pages 的仓库名称
-  assetPrefix: process.env.NODE_ENV === "production" ? "/taroblog-git-master-1940879828s-projects.vercel.app/" : "" // GitHub Pages 的仓库名称
+  /* config options here */
 }
 
 export default nextConfig
