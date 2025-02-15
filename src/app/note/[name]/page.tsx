c54d6a13e573f16d7f9b05caff3849c2e3ac306a
yejiechen <1940879828@qq.com>
2025-02-15 18:18:56 +0800

    feat: 文章详情显示title

--- src/app/note/[name]/page.tsx
+++ src/app/note/[name]/page.tsx
@@ -21,19 +21,22 @@ async function getMarkdownContent(name: string) {
   const decodedName = decodeURIComponent(name) // 将路径参数编码回来
   const filePath = path.join(process.cwd(), "public/notes", `${decodedName}.md`)
   const fileContent = fs.readFileSync(filePath, "utf8")
-  const { content } = matter(fileContent) // 解析 Markdown 内容
-  return content
+  const { content,data } = matter(fileContent) // 解析 Markdown 内容
+  return {content,data}
 }
 
 export default async function NoteDetail({
   params
 }: { params: Promise<{ name: string }> }) {
   const { name } = await params
-  const content = await getMarkdownContent(name)
+  const {content,data} = await getMarkdownContent(name)
 
   return (
     <div className="w-container flex-nowrap flex flex-col">
       <BackButton className="mt-4" />
+      <div className="mt-2">
+        : {data.title}
+      </div>
       <Markdown content={content} />
     </div>
   )
